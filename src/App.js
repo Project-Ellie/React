d7fa03db362b67085db992ec041028553e61869b
Wolfgang Giersche <wgiersche@gmail.com>
2019-12-15 17:39:09 +0100

    First request to Django

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+const list = [
+  {
+    "id": 1,
+    "title": "1st todo",
+    "body": "Learn Django properly"
+  },
+  {
+    "id": 2,
+    "title": "2nd todo",
+    "body": "Learn Python properly"
+  },
+  {
+    "id": 3,
+    "title": "3rd todo",
+    "body": "Learn HTTP and REST properly"
+  }
+];*/
+
+
+class App extends Component {
+
+  state = {
+    'todos': []
+  };
+
+  componentDidMount() {
+    this.getTodos()
+  }
+
+  getTodos() {
+    axios
+        .get('http://127.0.0.1:8000/todosapi')
+        .then(res => {
+          this.setState({'todos': res.data})
+        })
+        .catch(err => {
+          console.log(err)
+        })
+  }
+
+  render(){
+    return(
+        <div>
+          {this.state.todos.map(item => (
+              <div key={item.id}>
+                <h1>{item.title}</h1>
+                <p>{item.body}</p>
+              </div>
+          ))}
+        </div>
+    )
+  }
 }
+
 
 export default App;
